8683d3a1c22ecc394c99ef845750827651394f70
Manish Pathak <immanish8521@gmail.com>
2021-02-04 15:25:53 +0530

    Updates

---
+++ precache-manifest.8289e236d6ccf819bc226066fe3ae1f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4101b65165db024c27ac609dcac5fef8",
+    "url": "/new-react-weather-app/index.html"
+  },
+  {
+    "revision": "fbd2d159b19d478b96ab",
+    "url": "/new-react-weather-app/static/css/main.3e2d8c15.chunk.css"
+  },
+  {
+    "revision": "9325eb656a779525a854",
+    "url": "/new-react-weather-app/static/js/2.b43eae92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-react-weather-app/static/js/2.b43eae92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbd2d159b19d478b96ab",
+    "url": "/new-react-weather-app/static/js/main.3fc7481d.chunk.js"
+  },
+  {
+    "revision": "3a4b8fa90e7b8d237afa",
+    "url": "/new-react-weather-app/static/js/runtime-main.1a047af8.js"
+  }
+]);
